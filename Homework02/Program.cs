80d349d05241eda7eab488a94c16c7e57f09288d
MuhammadaliUsa <128176737+MuhammadaliUsa@users.noreply.github.com>
2023-03-31 09:29:23 +0500

    task2

--- Homework02/Program.cs
+++ Homework02/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number, fact = 1;
+            Console.Write("Enter the your number: ");
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                for (int i = 1; i <= number; i++)
+                {
+                    fact *= i;
+                }
+                Console.WriteLine($"Number is factorial: {fact}");
+            }
+            else
+            {
+                Console.WriteLine("Please enter the number!");
+            }
+            Console.WriteLine();
+            Main(args);
         }
     }
 }
