a15e8d69c4774e0117950ce0fde47b1dd1bce2a3
MuhammadaliUsa <128176737+MuhammadaliUsa@users.noreply.github.com>
2023-03-31 09:31:36 +0500

    task3

--- Homework02/Program.cs
+++ Homework02/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int number1, number2, number3, number4, max, min, difference;
+            Console.Write("Enter the first number: ");
+            int.TryParse(Console.ReadLine(), out number1);
+            Console.Write("Enter the second number: ");
+            int.TryParse(Console.ReadLine(), out number2);
+            Console.Write("Enter the three number: ");
+            int.TryParse(Console.ReadLine(), out number3);
+            Console.Write("Enter the four  number: ");
+            int.TryParse(Console.ReadLine(), out number4);
+            max = Math.Max(number1, Math.Max(number2, Math.Max(number3, number4)));
+            min = Math.Min(number1, Math.Min(number2, Math.Min(number3, number4)));
+            difference = max - min;
+            Console.WriteLine($"Diffirence Max and Min: {max - min}");
+            Console.WriteLine();
+            Main(args);
+
+
         }
     }
 }
