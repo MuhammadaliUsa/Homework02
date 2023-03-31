50a06b5ea3434de62fddcce622370e0a54f0769d
MuhammadaliUsa <128176737+MuhammadaliUsa@users.noreply.github.com>
2023-03-31 09:16:45 +0500

    initial commit

--- Homework02/Program.cs
+++ Homework02/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             ///01 Convert decimal to binary
+
+            Console.Write("Enter a decimal number: ");
+            int decimalNumber = int.Parse(Console.ReadLine());
+            string binaryNumber = Convert.ToString(decimalNumber, 2);
+            Console.WriteLine($"Binary representation: {binaryNumber}");
         }
     }
 }
