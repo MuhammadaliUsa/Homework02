212bd30e5bd1e68e11d75cd0645ad7f78fb8b651
MuhammadaliUsa <128176737+MuhammadaliUsa@users.noreply.github.com>
2023-03-31 09:36:22 +0500

    task7

--- Homework02/Program.cs
+++ Homework02/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int startNumber, endNumber, sum = 0;
+            Console.Write("Enter starting range: ");
+            int.TryParse(Console.ReadLine(), out startNumber);
+            Console.Write("Enter ending range: ");
+            int.TryParse(Console.ReadLine(), out endNumber);
+            for (int i = startNumber; i < endNumber; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        sum += j;
+                    }
+
+                }
+
+                if (sum == i)
+                {
+                    Console.WriteLine(i);
+                }
+                sum = 0;
+
+            }
+
         }
     }
 }
